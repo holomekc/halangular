4cdeb3d44d34d3157b662c4b60d0c52bd56b3ed1
Christopher <holomekc@gmail.com>
2018-08-23 21:39:41 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hal-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'halangular';
+}
